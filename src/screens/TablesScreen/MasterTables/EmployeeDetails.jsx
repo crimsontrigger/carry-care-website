1116942a27f94842f81753d0131d23ac285591ae
raf2k07 <raf2k07@gmail.com>
2020-12-23 03:26:24 +0530

    refactor all master tables to use CustomTable component.

--- src/screens/TablesScreen/MasterTables/EmployeeDetails.jsx
+++ src/screens/TablesScreen/MasterTables/EmployeeDetails.jsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Table } from "antd";
-import Button from "react-bootstrap/Button";
-import DataInputModal from "./../../../components/DataInputModal";
+import CustomTable from "./../../../components/CustomTable";
 import "./../styles.css";
 
 const EmployeeDetails = () => {
-  const [dataSource, setDataSource] = useState([]);
-  const [showDataForm, setShowDataForm] = useState(false);
+  const data = [];
 
   const columns = [
     {
@@ -36,60 +33,7 @@ const EmployeeDetails = () => {
     },
   ];
 
-  useEffect(() => {
-    console.log("data: ", dataSource);
-  }, [dataSource]);
-
-  const addRow = (rowData) => {
-    let tempData = dataSource;
-    rowData.key = tempData.length + 1;
-    tempData.push(rowData);
-    setDataSource([...tempData]);
-    console.log(dataSource);
-  };
-
-  const handleAddData = () => {
-    setShowDataForm(true);
-  };
-
-  const setModalVisible = (tempbool) => {
-    setShowDataForm(tempbool);
-  };
-
-  const rowSelection = {
-    onChange: (selectedRowKeys, selectedRows) => {
-      console.log(
-        `selectedRowKeys: ${selectedRowKeys}`,
-        "selectedRows: ",
-        selectedRows
-      );
-    },
-    // getCheckboxProps: (record) => ({
-    //   disabled: record.name === "Disabled User", // Column configuration not to be checked
-    //   name: record.name,
-    // }),
-  };
-
-  return (
-    <div style={{ width: "100%" }}>
-      <DataInputModal
-        addRow={addRow}
-        columns={columns}
-        setVisible={setModalVisible}
-        visible={showDataForm}
-      />
-      <div className={"table-actions-container"}>
-        <Button variant={"primary"} onClick={handleAddData}>
-          Add Row
-        </Button>
-      </div>
-      <Table
-        columns={columns}
-        dataSource={dataSource}
-        rowSelection={{ ...rowSelection }}
-      />
-    </div>
-  );
+  return <CustomTable tableColumns={columns} tableData={data} />;
 };
 
 export default EmployeeDetails;
